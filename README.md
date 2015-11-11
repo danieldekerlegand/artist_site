f13f40d728699e59fc4672b053bf88d308bc8285
ddeker <dldekerl@uno.edu>
2015-11-11 20:00:03 +0000

    fixed profile.ejs / commission tracker

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # artist_site
+
+This repository is a template for an artist website. The site uses a jQuery carousel app 
+to display an art gallery. It uses Passport authentication to provide a login for buyers 
+to track the process of their commissions. The commission-tracking app itself is 
+in the development stage.
+
+## Setup
+- Download and install node.js [https://nodejs.org/en/download/](https://nodejs.org/en/download/)
+- Execute at command line `npm install` to grab dependencies.
+
+### To run site/web-server locally
+- Run `npm start`
+- Open browser and navigate to [http://localhost:1337/](http://localhost:1337/)
--- db/users.js
+++ db/users.js
@@ -1,6 +1,6 @@
 var records = [
-    { id: 1, username: 'jack', password: 'secret', displayName: 'Jack', emails: [ { value: 'jack@example.com' } ] }
-  , { id: 2, username: 'jill', password: 'birthday', displayName: 'Jill', emails: [ { value: 'jill@example.com' } ] }
+    { id: 1, username: 'jack', password: 'secret', displayName: 'Jack', emails: [ { value: 'jack@example.com' } ], commission: 'Portrait', comImage: 'http://40.media.tumblr.com/dbc060e95cda07fa69aad4e4ccfbd3c1/tumblr_n2268zqoPA1ru9006o1_500.jpg'}
+  , { id: 2, username: 'jill', password: 'birthday', displayName: 'Jill', emails: [ { value: 'jill@example.com' } ] , commission: 'Cleopatra', comImage: 'http://40.media.tumblr.com/7012445c0bd5cd03d00b4436c1f0ac35/tumblr_mxuq3w3Hkb1ru9006o1_500.jpg'}
 ];
 
 exports.findById = function(id, cb) {
